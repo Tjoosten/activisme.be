c42ed03d3bcab0b458d1387bbb5141bb0a0d894d
Tim Joosten <Tjoosten3@gmail.com>
2017-04-27 23:05:23 +0200

    Empty changes.sql after SD4.2

--- changes.sql
+++ changes.sql
@@ -4,9 +4,3 @@
 --
 -- Deze wijzigingen worden doorgevoerd bij elke deployment.
 -- Na de deployment wijziging word deze file geleegd.
-
--- FIX FOR #44 [DATABASE]: Rename session db table.
-RENAME TABLE ci_sessions TO  sessions_index;
-
--- FIX FOR #38 Mogelijk aanpassen volgorde van de lijsten
-ALTER TABLE links ADD end_date VARCHAR(60) AFTER author_id;
