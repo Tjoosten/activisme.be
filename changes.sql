5aa90c4b50388f721cc6a2431652c00bf642cc38
Tim Joosten <Tjoosten3@gmail.com>
2017-04-27 18:55:02 +0200

    Fix #53 Implement changes.sql

---
+++ changes.sql
@@ -0,0 +1,4 @@
+-- Schrijf hier de back-end wijzigings queries aan de database.
+--
+-- Deze wijzigingen worden doorgevoerd bij elke deployment.
+-- Na de deployment wijziging word deze file geleegd.
