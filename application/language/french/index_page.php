140cbf3ed61af87cbe48c3a4fee8148b1e8e0e79
Tim Joosten <Tjoosten3@gmail.com>
2017-05-30 12:24:15 +0200

    Fix #74 Vertalingen index

---
+++ application/language/french/index_page.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['title_1']  = "Nous sommes qui?";
+$lang['list_1_1'] = "On est une petite équipe qui défend les intérêts de droits d' hommes et des enfants.";
+$lang['list_1_2'] = "Nous sommes actif sur le web, autant que sur la rue, avec des actions ludiques de paix.";
+$lang['list_1_3'] = "Notre platforme existe d' une petite équipe, qui est désormais très actif, nous sommes tous des volontaires, qui s' y mettent  à 100%.";
+$lang['list_1_4'] = "On est  indépendant des parties, organisations politiques et autres!";
+$lang['list_1_5'] = "Nous survivons avec l' aide des volontaires et les donations des gens qui supportent notre cause.";
+$lang['list_1_6'] = "Activisme_be à été réalisé par Tom Manhaeghe.";
+
+$lang['title_2']  = "What do we do?";
+
+$lang['list_2_1'] = "Notre bût est de rassembler des gens, ainsi que des organisations pour lutter pour les droits humanitaires.";
+$lang['list_2_2'] = "Sur le web, on fait des pétitions, et autres... avec le support de notre web-team. Celles-ci les fournissent gratuitement aux organisations qui s'y intéressent.";
+$lang['list_2_3'] = "À part d' activisme on à aussi un mouvement qui donne nourriture, vêtements, et dés nécessités urgents pour les pauvres, en manques et les sans abris.";
+$lang['list_2_4'] = "Nous offrons aussi des  moments éducatives comme des lectures, assemblées, etc.";
+$lang['list_2_5'] = "Activisme_be à été réalisé par Tom Manhaeghe";
